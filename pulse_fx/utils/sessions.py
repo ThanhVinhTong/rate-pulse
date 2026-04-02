d32d9755a1a53fb2e3f23854cf6bba719b20403b
ThanhVinhTong <vinhtongthanh57@gmail.com>
2026-04-02 22:08:31 +0800

    fix: changed OS

--- pulse_fx/utils/sessions.py
+++ pulse_fx/utils/sessions.py
@@ -54,7 +54,8 @@ def start_driver(webdriver_path):
         options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
         options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36')
         
-        service = EdgeService(executable_path=webdriver_path)
+        # service = EdgeService(executable_path=webdriver_path)
+        service = EdgeService()
         try:
             driver = webdriver.Edge(service=service, options=options)
         except WebDriverException:
